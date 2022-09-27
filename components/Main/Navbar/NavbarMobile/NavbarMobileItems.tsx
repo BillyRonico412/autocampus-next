b6820dcdc9ee50211809dca9ab8de86868fcc4b8
BillyRonico <billyronico412@gmail.com>
2022-09-28 00:09:02 +0200

    Add zindex navbar

--- components/Main/Navbar/NavbarMobile/NavbarMobileItems.tsx
+++ components/Main/Navbar/NavbarMobile/NavbarMobileItems.tsx
@@ -3,7 +3,7 @@ import NavbarMobileItem1 from "./NavbarMobileItem1";
 
 const NavbarMobileItems = () => {
 	return (
-		<div className="text-xl font-semibold absolute w-full bg-white z-10">
+		<div className="text-xl font-semibold absolute w-full bg-white z-50">
 			{menuItems.map((menuItem, i) => (
 				<NavbarMobileItem1 menuItem={menuItem} index={i} key={i} />
 			))}
